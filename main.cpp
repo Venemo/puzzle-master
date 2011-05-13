53998bc63cea23772d9c7af56ab0b65cb3033c4f
venemo <venemo@msn.com>
2011-05-14 01:49:45 +0200

    The application is now translateable. Added Hungarian translation to the resource file.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationName("Venemo");
     QApplication::setApplicationVersion(QString(APP_VERSION));
 
+    QTranslator tQt, tApp;
+    tQt.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    tApp.load("puzzle-master_" + QLocale::system().name(), ":/translations");
+
+    qDebug() << QLocale::system().name();
+
+    QApplication::installTranslator(&tQt);
+    QApplication::installTranslator(&tApp);
+
     qsrand((uint)QTime::currentTime().msec());
     settings = new QSettings();
 
