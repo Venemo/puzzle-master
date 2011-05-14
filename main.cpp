874d4647a30203ea1bbbc82931a570bb81dc1b36
venemo <venemo@msn.com>
2011-05-14 03:06:29 +0200

    * Misc packaging info changes * Using LANG * Bumped version number to 1.2.3

--- main.cpp
+++ main.cpp
@@ -13,14 +13,19 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationName("Venemo");
     QApplication::setApplicationVersion(QString(APP_VERSION));
 
-    QTranslator tQt, tApp;
-    tQt.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    tApp.load("puzzle-master_" + QLocale::system().name(), ":/translations");
+    QString langCode;
+    langCode = getenv("LANG");
+    if (langCode.isEmpty() || langCode == "C")
+        langCode = QLocale::system().name();
 
-    //qDebug() << QLocale::system().name();
-
-    QApplication::installTranslator(&tQt);
-    QApplication::installTranslator(&tApp);
+    if (!langCode.isEmpty() && langCode != "C")
+    {
+        QTranslator tQt, tApp;
+        tQt.load("qt_" + langCode, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+        tApp.load("puzzle-master_" + langCode, ":/translations");
+        QApplication::installTranslator(&tQt);
+        QApplication::installTranslator(&tApp);
+    }
 
     qsrand((uint)QTime::currentTime().msec());
     settings = new QSettings();
