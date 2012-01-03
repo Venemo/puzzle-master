2e7f70bf28dd24a28575496786afe8408566e383
Timur Kristóf <venemo@msn.com>
2012-01-03 20:59:13 +0100

    Forgot to add the files...

---
+++ translations/puzzle-master_he_IL.ts
@@ -0,0 +1,282 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="he_IL">
+<context>
+    <name>AppWindow</name>
+    <message>
+        <source>You&apos;re a winner!</source>
+        <translation>זכית!</translation>
+    </message>
+    <message>
+        <source>Congratulations, you have successfully solved the given puzzle.</source>
+        <translation>כל הכבוד! פתרת בהצלחה את הפאזל</translation>
+    </message>
+    <message>
+        <source>Play again</source>
+        <translation>שחק שנית</translation>
+    </message>
+    <message>
+        <source>Choose other</source>
+        <translation>בחירת אחר</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation>האם הנכם בטוחים?</translation>
+    </message>
+    <message>
+        <source>Do you wish to quit the game?</source>
+        <translation>האם ברצונך לעזוב את המשחק?</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>כן</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>לא</translation>
+    </message>
+    <message>
+        <source>About Puzzle Master %1</source>
+        <translation>אודות פאזל מאסטר %1</translation>
+    </message>
+    <message>
+        <source>Brought to you by Timur Kristóf.
+Puzzle Master is a fun and addictive jigsaw puzzle game.
+Using Qt.</source>
+        <translation>נוצר ע&quot;י טימור קריסטוף.
+פאזל מאסטר הינו משחק תצרף (פאזל) מהנה וממכר.
+על בסיס תשתית Qt</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>בסדר</translation>
+    </message>
+    <message>
+        <source>Isn&apos;t this a bit difficult?</source>
+        <translation>האין זה קשה במעט?</translation>
+    </message>
+    <message>
+        <source>The selected options will make the game VERY difficult.
+Are you sure to proceed?</source>
+        <translation>האפשרויות שנבחרו יהפכו את המשחק לקשה מאוד.
+האם ברצונכם להמשיך?</translation>
+    </message>
+    <message>
+        <source>You have already added the selected image.</source>
+        <translation>התמונה שנבחרה כבר נוספה</translation>
+    </message>
+</context>
+<context>
+    <name>FileSelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation>בחרו תמונה</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>בחר</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+    <message>
+        <source>An error has occoured</source>
+        <translation>התרחשה תקלה</translation>
+    </message>
+</context>
+<context>
+    <name>GallerySelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation>בחרו תמונה</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>בחר</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+</context>
+<context>
+    <name>GameBoard</name>
+    <message>
+        <source>Creating puzzle piece %1 of %2</source>
+        <translation>יוצר פיסת תצרף מס&apos; %1 מתוך %2</translation>
+    </message>
+    <message>
+        <source>The selected image is being processed.</source>
+        <translation>התמונה שנבחרה בתהליכי עיבוד</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Wait...</source>
+        <translation>המתינו...</translation>
+    </message>
+    <message>
+        <source>Continue game</source>
+        <translation>המשך משחק</translation>
+    </message>
+    <message>
+        <source>Get a hint!</source>
+        <translation>רמז!</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>איתחול</translation>
+    </message>
+    <message>
+        <source>Surrender</source>
+        <translation>כניעה</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>אודות</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>יציאה</translation>
+    </message>
+    <message>
+        <source>Please wait...</source>
+        <translation>אנא המתינו...</translation>
+    </message>
+    <message>
+        <source>Game paused</source>
+        <translation>משחק בהמתנה</translation>
+    </message>
+    <message>
+        <source>Switch back to continue it.</source>
+        <translation>חיזרו על מנת להמשיך</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation>האם הנכם בטוחים?</translation>
+    </message>
+    <message>
+        <source>Do you want to give up this game?
+You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
+        <translation>האם ברצונכם לוותר על המשחק?
+אם כן, לא תוכלו להמשיך. השתמשו ב&quot;רמז!&quot; אם ברצונכם לקבל רמז.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>כן</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>לא</translation>
+    </message>
+    <message>
+        <source>Do you want to restart this game?</source>
+        <translation>האם ברצונכם להתחיל את המשחק מחדש?</translation>
+    </message>
+    <message>
+        <source>An error has occoured</source>
+        <translation>התרחשה תקלה</translation>
+    </message>
+    <message>
+        <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
+        <translation>סליחה, לא הצלחנו להתחיל את המשחק. אנא נסו שנית עם תמונה אחרת.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>בסדר</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation>פאזל מאסטר</translation>
+    </message>
+</context>
+<context>
+    <name>ImageChooser</name>
+    <message>
+        <source>Ok</source>
+        <translation>בסדר</translation>
+    </message>
+    <message>
+        <source>Welcome! Choose an image.</source>
+        <translation>ברוכים הבאים! ביחרו תמונה</translation>
+    </message>
+    <message>
+        <source>Start game</source>
+        <translation>התחל משחק</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Please choose</source>
+        <translation>אנא ביחרו</translation>
+    </message>
+    <message>
+        <source>You must choose an image before continuing.</source>
+        <translation>עליכם לבחור תמונה בטרם תוכלו להמשיך</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>אודות</translation>
+    </message>
+    <message>
+        <source>Add custom image</source>
+        <translation>הוסף תמונה</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>יציאה</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation>פאזל מאסטר</translation>
+    </message>
+    <message>
+        <source>Remove custom image</source>
+        <translation>הסר תמונה</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>Game difficulty</source>
+        <translation>קושי משחק</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>שחק</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>ביטול</translation>
+    </message>
+    <message>
+        <source>Columns</source>
+        <translation>עמודות</translation>
+    </message>
+    <message>
+        <source>Rows</source>
+        <translation>שורות</translation>
+    </message>
+    <message>
+        <source>Snap</source>
+        <translation>שבור</translation>
+    </message>
+    <message>
+        <source>Easy</source>
+        <translation>קל</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>בינוני</translation>
+    </message>
+    <message>
+        <source>Difficult</source>
+        <translation>קשה</translation>
+    </message>
+</context>
+</TS>
---
+++ update.sh
@@ -0,0 +1,2 @@
+lupdate-qt4 -no-obsolete */*/*.qml */*/*/*.qml */*/*/*/*.qml -ts translations/*.ts
+lrelease-qt4 translations/*.ts
