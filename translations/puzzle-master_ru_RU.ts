b39848bd54e02daa02c6e220b9c661ea898f7d2d
Timur Kristóf <venemo@msn.com>
2012-01-03 16:35:14 +0100

    Russian translation added, thanks to ZogG :)

---
+++ translations/puzzle-master_ru_RU.ts
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
+<context>
+    <name>AppWindow</name>
+    <message>
+        <source>You&apos;re a winner!</source>
+        <translation type="obsolete">Вы победитель!</translation>
+    </message>
+    <message>
+        <source>Congratulations, you have successfully solved the given puzzle.</source>
+        <translation type="obsolete">Поздравляю, вы успешно разрешили загадку.</translation>
+    </message>
+    <message>
+        <source>Play again</source>
+        <translation type="obsolete">Еще раз</translation>
+    </message>
+    <message>
+        <source>Choose other</source>
+        <translation type="obsolete">Выбрать другую</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation type="obsolete">Вы уверены?</translation>
+    </message>
+    <message>
+        <source>Do you wish to quit the game?</source>
+        <translation type="obsolete">Вы хотите покинуть игру?</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="obsolete">Да</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="obsolete">Нет</translation>
+    </message>
+    <message>
+        <source>About Puzzle Master %1</source>
+        <translation type="obsolete">О программе Puzzle Master %1</translation>
+    </message>
+    <message>
+        <source>Brought to you by Timur Kristóf.
+Puzzle Master is a fun and addictive jigsaw puzzle game.
+Using Qt.</source>
+        <translation type="obsolete">Автор Timur Kristóf.
+A Puzzle Master веселая и увлекательная игра головоломок.
+Написанная на Qt.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation type="obsolete">Ок</translation>
+    </message>
+    <message>
+        <source>Isn&apos;t this a bit difficult?</source>
+        <translation type="obsolete">Не слишком ли сложно?</translation>
+    </message>
+    <message>
+        <source>The selected options will make the game VERY difficult.
+Are you sure to proceed?</source>
+        <translation type="obsolete">Вы выбрали ОЧЕНЬ сложный уровень.
+Вы уверенны?</translation>
+    </message>
+</context>
+<context>
+    <name>FileSelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation type="obsolete">Выбрать картинку</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation type="obsolete">Выбрать</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>GallerySelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation type="obsolete">Выбрать картинку</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation type="obsolete">Выбрать</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>GameBoard</name>
+    <message>
+        <source>Creating puzzle piece %1 of %2</source>
+        <translation type="obsolete">Загружаем картику (%1 из %2) </translation>
+    </message>
+    <message>
+        <source>The selected image is being processed.</source>
+        <translation type="obsolete">Загрузка выбранной картинки</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="obsolete">...</translation>
+    </message>
+    <message>
+        <source>Wait...</source>
+        <translation type="obsolete">Подождите...</translation>
+    </message>
+    <message>
+        <source>Continue game</source>
+        <translation type="obsolete">Продолжить</translation>
+    </message>
+    <message>
+        <source>Get a hint!</source>
+        <translation type="obsolete">Подсказка!</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation type="obsolete">Рестарт</translation>
+    </message>
+    <message>
+        <source>Surrender</source>
+        <translation type="obsolete">Сдаться</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="obsolete">О программе</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="obsolete">Выйти</translation>
+    </message>
+    <message>
+        <source>Please wait...</source>
+        <translation type="obsolete">Подождите пожалуйста...</translation>
+    </message>
+    <message>
+        <source>Game paused</source>
+        <translation type="obsolete">Пауза</translation>
+    </message>
+    <message>
+        <source>Switch back to continue it.</source>
+        <translation type="obsolete">Вернуться.</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation type="obsolete">Вы уверены?</translation>
+    </message>
+    <message>
+        <source>Do you want to give up this game?
+You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
+        <translation type="obsolete">Вы сдаетесь?
+Вы не сможете продолжить если покините игру. Воспользуйтесь подсказкой, если нужна помощь.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="obsolete">Да</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation type="obsolete">Нет</translation>
+    </message>
+    <message>
+        <source>Do you want to restart this game?</source>
+        <translation type="obsolete">Начать эту игру заново?</translation>
+    </message>
+    <message>
+        <source>An error has occoured</source>
+        <translation type="obsolete">Произошла ошибка!</translation>
+    </message>
+    <message>
+        <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
+        <translation type="obsolete">Извините, но нам не удалось запустить игру. Выберите другую картинку.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation type="obsolete">Ок</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation type="obsolete">Puzzle Master</translation>
+    </message>
+</context>
+<context>
+    <name>ImageChooser</name>
+    <message>
+        <source>Ok</source>
+        <translation type="obsolete">Ок</translation>
+    </message>
+    <message>
+        <source>Welcome! Choose an image.</source>
+        <translation type="obsolete">Добро пожаловать! Выберите картинку.</translation>
+    </message>
+    <message>
+        <source>Start game</source>
+        <translation type="obsolete">Начать игру</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation type="obsolete">...</translation>
+    </message>
+    <message>
+        <source>Please choose</source>
+        <translation type="obsolete">Выберите!</translation>
+    </message>
+    <message>
+        <source>You must choose an image before continuing.</source>
+        <translation type="obsolete">Вы должны сначала выбрать картинку.</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="obsolete">О программе</translation>
+    </message>
+    <message>
+        <source>Add custom image</source>
+        <translation type="obsolete">Добавить свою картинку</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation type="obsolete">Выйти</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation type="obsolete">Puzzle Master</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>Game difficulty</source>
+        <translation type="obsolete">Сложность</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation type="obsolete">Играть</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">Отмена</translation>
+    </message>
+    <message>
+        <source>Columns</source>
+        <translation type="obsolete">Колонки</translation>
+    </message>
+    <message>
+        <source>Rows</source>
+        <translation type="obsolete">Столбики</translation>
+    </message>
+    <message>
+        <source>Snap</source>
+        <translation type="obsolete">Перемешать</translation>
+    </message>
+    <message>
+        <source>Easy</source>
+        <translation type="obsolete">Лекго</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation type="obsolete">Нормально</translation>
+    </message>
+    <message>
+        <source>Difficult</source>
+        <translation type="obsolete">Сложно</translation>
+    </message>
+</context>
+</TS>
