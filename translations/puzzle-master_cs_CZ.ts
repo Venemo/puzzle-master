c7d1a40666e31ff5413647d7c82cd3743210e68e
Timur Kristóf <venemo@msn.com>
2012-01-03 16:30:22 +0100

    Added empty translation files for Italian and Czech

---
+++ translations/puzzle-master_cs_CZ.ts
@@ -0,0 +1,342 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>AppWindow</name>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="58"/>
+        <source>You&apos;re a winner!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="59"/>
+        <source>Congratulations, you have successfully solved the given puzzle.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="61"/>
+        <source>Play again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="62"/>
+        <source>Choose other</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="74"/>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="75"/>
+        <source>Do you wish to quit the game?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="76"/>
+        <location filename="../qml/other/AppWindow.qml" line="99"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="77"/>
+        <location filename="../qml/other/AppWindow.qml" line="100"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="82"/>
+        <source>About Puzzle Master %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="83"/>
+        <source>Brought to you by Timur Kristóf.
+Puzzle Master is a fun and addictive jigsaw puzzle game.
+Using Qt.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="84"/>
+        <location filename="../qml/other/AppWindow.qml" line="108"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="97"/>
+        <source>Isn&apos;t this a bit difficult?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="98"/>
+        <source>The selected options will make the game VERY difficult.
+Are you sure to proceed?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/AppWindow.qml" line="107"/>
+        <source>You have already added the selected image.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileSelectorDialog</name>
+    <message>
+        <location filename="../qml/other/FileSelectorDialog.qml" line="29"/>
+        <source>Select a picture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/FileSelectorDialog.qml" line="30"/>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/FileSelectorDialog.qml" line="31"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GallerySelectorDialog</name>
+    <message>
+        <location filename="../qml/other/GallerySelectorDialog.qml" line="29"/>
+        <source>Select a picture</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GallerySelectorDialog.qml" line="30"/>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GallerySelectorDialog.qml" line="31"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GameBoard</name>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="45"/>
+        <source>Creating puzzle piece %1 of %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="47"/>
+        <source>The selected image is being processed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="94"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="104"/>
+        <source>Puzzle Master</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="122"/>
+        <source>Wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="122"/>
+        <source>Continue game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="122"/>
+        <source>Get a hint!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="145"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="150"/>
+        <source>Surrender</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="156"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="164"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="176"/>
+        <source>Please wait...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="189"/>
+        <source>Game paused</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="190"/>
+        <source>Switch back to continue it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="208"/>
+        <location filename="../qml/other/GameBoard.qml" line="221"/>
+        <source>Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="209"/>
+        <source>Do you want to give up this game?
+You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="210"/>
+        <location filename="../qml/other/GameBoard.qml" line="223"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="211"/>
+        <location filename="../qml/other/GameBoard.qml" line="224"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="222"/>
+        <source>Do you want to restart this game?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="235"/>
+        <source>An error has occoured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="236"/>
+        <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/GameBoard.qml" line="237"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageChooser</name>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="73"/>
+        <source>Welcome! Choose an image.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="164"/>
+        <source>Start game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="181"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="190"/>
+        <source>Please choose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="191"/>
+        <source>You must choose an image before continuing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="192"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="196"/>
+        <source>Puzzle Master</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="205"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="213"/>
+        <source>Add custom image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="222"/>
+        <source>Remove custom image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/ImageChooser.qml" line="231"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="28"/>
+        <source>Game difficulty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="29"/>
+        <source>Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="30"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="43"/>
+        <source>Columns</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="75"/>
+        <source>Rows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="107"/>
+        <source>Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
+        <source>Easy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
+        <source>Difficult</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
