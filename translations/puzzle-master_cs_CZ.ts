6b037b26c24179d909ac221647fd8110c01d2914
Timur Kristóf <venemo@msn.com>
2012-01-08 13:26:50 +0100

    Adjusted Czech translation

--- translations/puzzle-master_cs_CZ.ts
+++ translations/puzzle-master_cs_CZ.ts
@@ -4,54 +4,42 @@
 <context>
     <name>AppWindow</name>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="58"/>
         <source>You&apos;re a winner!</source>
         <translation>Zvítězil(a) jste!</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="59"/>
         <source>Congratulations, you have successfully solved the given puzzle.</source>
         <translation>Gratuluji, úspěšně jste vyřešil(a) zadané puzzle.</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="61"/>
         <source>Play again</source>
         <translation>Hrát znovu</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="62"/>
         <source>Choose other</source>
         <translation>Vybrat jiné</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="74"/>
         <source>Are you sure?</source>
         <translation>Opravdu ukončit hru?</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="75"/>
         <source>Do you wish to quit the game?</source>
         <translation>Chcete ukončit hru?</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="76"/>
-        <location filename="../qml/other/AppWindow.qml" line="99"/>
         <source>Yes</source>
         <translation>Ano</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="77"/>
-        <location filename="../qml/other/AppWindow.qml" line="100"/>
         <source>No</source>
         <translation>Ne</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="82"/>
         <source>About Puzzle Master %1</source>
         <translation>O hře Puzzle Master %1</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="83"/>
         <source>Brought to you by Timur Kristóf.
 Puzzle Master is a fun and addictive jigsaw puzzle game.
 Using Qt.</source>
@@ -60,25 +48,20 @@ Puzzle Master je zábavná a návyková puzzle hra.
 Za využití Qt.</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="84"/>
-        <location filename="../qml/other/AppWindow.qml" line="108"/>
         <source>Ok</source>
         <translation>Ok</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="97"/>
         <source>Isn&apos;t this a bit difficult?</source>
         <translation>Trochu složité, ne?</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="98"/>
         <source>The selected options will make the game VERY difficult.
 Are you sure to proceed?</source>
         <translation>Zvolené nastavení učiní hru VELMI obtížnou.
 Opravdu chcete pokračovat?</translation>
     </message>
     <message>
-        <location filename="../qml/other/AppWindow.qml" line="107"/>
         <source>You have already added the selected image.</source>
         <translation>Vybraný obrázek byl již přidán.</translation>
     </message>
@@ -86,40 +69,33 @@ Opravdu chcete pokračovat?</translation>
 <context>
     <name>FileSelectorDialog</name>
     <message>
-        <location filename="../qml/other/FileSelectorDialog.qml" line="29"/>
         <source>Select a picture</source>
         <translation>Vyberte obrázek</translation>
     </message>
     <message>
-        <location filename="../qml/other/FileSelectorDialog.qml" line="30"/>
         <source>Select</source>
         <translation>Vybrat</translation>
     </message>
     <message>
-        <location filename="../qml/other/FileSelectorDialog.qml" line="31"/>
         <source>Cancel</source>
         <translation>Storno</translation>
     </message>
     <message>
-        <location filename="../qml/other/FileSelectorDialog.qml" line="88"/>
         <source>An error has occoured</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastala chyba</translation>
     </message>
 </context>
 <context>
     <name>GallerySelectorDialog</name>
     <message>
-        <location filename="../qml/other/GallerySelectorDialog.qml" line="29"/>
         <source>Select a picture</source>
         <translation>Vyberte obrázek</translation>
     </message>
     <message>
-        <location filename="../qml/other/GallerySelectorDialog.qml" line="30"/>
         <source>Select</source>
         <translation>Vybrat</translation>
     </message>
     <message>
-        <location filename="../qml/other/GallerySelectorDialog.qml" line="31"/>
         <source>Cancel</source>
         <translation>Storno</translation>
     </message>
@@ -127,116 +103,91 @@ Opravdu chcete pokračovat?</translation>
 <context>
     <name>GameBoard</name>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="45"/>
         <source>Creating puzzle piece %1 of %2</source>
         <translation>Připravuji %1 dílek puzzle z %2</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="47"/>
         <source>The selected image is being processed.</source>
         <translation>Vybraný obrázek se právě zpracovává.</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="94"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="104"/>
         <source>Puzzle Master</source>
         <translation>Puzzle Master</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="122"/>
         <source>Wait...</source>
         <translation>Čekejte...</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="122"/>
         <source>Continue game</source>
         <translation>Pokračovat ve hře</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="122"/>
         <source>Get a hint!</source>
         <translation>Poradit!</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="145"/>
         <source>Restart</source>
         <translation>Začít znovu</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="150"/>
         <source>Surrender</source>
         <translation>Vzdát se</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="156"/>
         <source>About</source>
         <translation>O hře</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="164"/>
         <source>Quit</source>
         <translation>Ukončit</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="176"/>
         <source>Please wait...</source>
         <translation>Prosím čekejte...</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="189"/>
         <source>Game paused</source>
         <translation>Hra pozastavena</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="190"/>
         <source>Switch back to continue it.</source>
         <translation>Pro pokračování se přepněte zpět do hry.</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="208"/>
-        <location filename="../qml/other/GameBoard.qml" line="221"/>
         <source>Are you sure?</source>
         <translation>Vzdát hru?</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="209"/>
         <source>Do you want to give up this game?
 You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
         <translation>Chcete opravdu vzdát hru? Nebudete v ní moci pokračovat. Pokud potřebujete nakopnout, použijte &quot;Poradit!&quot;.</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="210"/>
-        <location filename="../qml/other/GameBoard.qml" line="223"/>
         <source>Yes</source>
         <translation>Ano</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="211"/>
-        <location filename="../qml/other/GameBoard.qml" line="224"/>
         <source>No</source>
         <translation>Ne</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="222"/>
         <source>Do you want to restart this game?</source>
         <translation>Chcete znovu začít tuhle hru?</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="235"/>
         <source>An error has occoured</source>
         <translation>Nastala chyba</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="236"/>
         <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
         <translation>Nemohu začít hru. Zkuste prosím vybrat jiný obrázek.</translation>
     </message>
     <message>
-        <location filename="../qml/other/GameBoard.qml" line="237"/>
         <source>Ok</source>
         <translation>Dobře</translation>
     </message>
@@ -244,57 +195,46 @@ You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.
 <context>
     <name>ImageChooser</name>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="73"/>
         <source>Welcome! Choose an image.</source>
         <translation>Vítejte! Vyberte si obrázek.</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="174"/>
         <source>Start game</source>
         <translation>Začít hru</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="191"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="200"/>
         <source>Please choose</source>
         <translation>Prosím vyberte obrázek</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="201"/>
         <source>You must choose an image before continuing.</source>
         <translation>Než budete moci pokračovat, musíte si vybrat obrázek puzzle.</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="202"/>
         <source>Ok</source>
         <translation>Dobře</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="206"/>
         <source>Puzzle Master</source>
         <translation>Puzzle Master</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="215"/>
         <source>About</source>
         <translation>O hře</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="223"/>
         <source>Add custom image</source>
         <translation>Přidat vlastní obrázek</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="232"/>
         <source>Remove custom image</source>
         <translation>Odstranit vlastní obrázek</translation>
     </message>
     <message>
-        <location filename="../qml/other/ImageChooser.qml" line="242"/>
         <source>Quit</source>
         <translation>Ukončit</translation>
     </message>
@@ -302,47 +242,38 @@ You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.
 <context>
     <name>OptionsDialog</name>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="28"/>
         <source>Game difficulty</source>
         <translation>Obtížnost hry</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="29"/>
         <source>Play</source>
         <translation>Hrát</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="30"/>
         <source>Cancel</source>
         <translation>Storno</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="43"/>
         <source>Columns</source>
         <translation>Sloupce</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="75"/>
         <source>Rows</source>
         <translation>Řádky</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="107"/>
         <source>Snap</source>
         <translation>Přichytávání</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
         <source>Easy</source>
         <translation>Jednoduché</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
         <source>Normal</source>
         <translation>Normální</translation>
     </message>
     <message>
-        <location filename="../qml/other/OptionsDialog.qml" line="131"/>
         <source>Difficult</source>
         <translation>Složité</translation>
     </message>
