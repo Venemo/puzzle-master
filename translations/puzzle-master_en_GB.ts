f544e57843cb6fbb58ed78cce44e5ac98fed5f6b
Timur Kristóf <venemo@msn.com>
2012-01-08 13:59:52 +0100

    Added English "translation"...

---
+++ translations/puzzle-master_en_GB.ts
@@ -0,0 +1,282 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB">
+<context>
+    <name>AppWindow</name>
+    <message>
+        <source>You&apos;re a winner!</source>
+        <translation>You&apos;re a winner!</translation>
+    </message>
+    <message>
+        <source>Congratulations, you have successfully solved the given puzzle.</source>
+        <translation>Congratulations, you have successfully solved the given puzzle.</translation>
+    </message>
+    <message>
+        <source>Play again</source>
+        <translation>Play again</translation>
+    </message>
+    <message>
+        <source>Choose other</source>
+        <translation>Choose other</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation>Are you sure?</translation>
+    </message>
+    <message>
+        <source>Do you wish to quit the game?</source>
+        <translation>Do you wish to quit the game?</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <source>About Puzzle Master %1</source>
+        <translation>About Puzzle Master %1</translation>
+    </message>
+    <message>
+        <source>Brought to you by Timur Kristóf.
+Puzzle Master is a fun and addictive jigsaw puzzle game.
+Using Qt.</source>
+        <translation>Brought to you by Timur Kristóf.
+Puzzle Master is a fun and addictive jigsaw puzzle game.
+Using Qt.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Isn&apos;t this a bit difficult?</source>
+        <translation>Isn&apos;t this a bit difficult?</translation>
+    </message>
+    <message>
+        <source>The selected options will make the game VERY difficult.
+Are you sure to proceed?</source>
+        <translation>The selected options will make the game VERY difficult.
+Are you sure to proceed?</translation>
+    </message>
+    <message>
+        <source>You have already added the selected image.</source>
+        <translation>You have already added the selected image.</translation>
+    </message>
+</context>
+<context>
+    <name>FileSelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation>Select a picture</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <source>An error has occoured</source>
+        <translation>An error has occoured</translation>
+    </message>
+</context>
+<context>
+    <name>GallerySelectorDialog</name>
+    <message>
+        <source>Select a picture</source>
+        <translation>Select a picture</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+</context>
+<context>
+    <name>GameBoard</name>
+    <message>
+        <source>Creating puzzle piece %1 of %2</source>
+        <translation>Creating puzzle piece %1 of %2</translation>
+    </message>
+    <message>
+        <source>The selected image is being processed.</source>
+        <translation>The selected image is being processed.</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation>Puzzle Master</translation>
+    </message>
+    <message>
+        <source>Wait...</source>
+        <translation>Wait...</translation>
+    </message>
+    <message>
+        <source>Continue game</source>
+        <translation>Continue game</translation>
+    </message>
+    <message>
+        <source>Get a hint!</source>
+        <translation>Get a hint!</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Surrender</source>
+        <translation>Surrender</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <source>Please wait...</source>
+        <translation>Please wait...</translation>
+    </message>
+    <message>
+        <source>Game paused</source>
+        <translation>Game paused</translation>
+    </message>
+    <message>
+        <source>Switch back to continue it.</source>
+        <translation>Switch back to continue it.</translation>
+    </message>
+    <message>
+        <source>Are you sure?</source>
+        <translation>Are you sure?</translation>
+    </message>
+    <message>
+        <source>Do you want to give up this game?
+You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
+        <translation>Do you want to give up this game?
+You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <source>Do you want to restart this game?</source>
+        <translation>Do you want to restart this game?</translation>
+    </message>
+    <message>
+        <source>An error has occoured</source>
+        <translation>An error has occoured</translation>
+    </message>
+    <message>
+        <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
+        <translation>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+</context>
+<context>
+    <name>ImageChooser</name>
+    <message>
+        <source>Welcome! Choose an image.</source>
+        <translation>Welcome! Choose an image.</translation>
+    </message>
+    <message>
+        <source>Start game</source>
+        <translation>Start game</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <source>Please choose</source>
+        <translation>Please choose</translation>
+    </message>
+    <message>
+        <source>You must choose an image before continuing.</source>
+        <translation>You must choose an image before continuing.</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Puzzle Master</source>
+        <translation>Puzzle Master</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <source>Add custom image</source>
+        <translation>Add custom image</translation>
+    </message>
+    <message>
+        <source>Remove custom image</source>
+        <translation>Remove custom image</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <source>Game difficulty</source>
+        <translation>Game difficulty</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Play</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <source>Columns</source>
+        <translation>Columns</translation>
+    </message>
+    <message>
+        <source>Rows</source>
+        <translation>Rows</translation>
+    </message>
+    <message>
+        <source>Snap</source>
+        <translation>Snap</translation>
+    </message>
+    <message>
+        <source>Easy</source>
+        <translation>Easy</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <source>Difficult</source>
+        <translation>Difficult</translation>
+    </message>
+</context>
+</TS>
