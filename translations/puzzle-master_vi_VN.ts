7d0061867b194bff1bff62f67b8d2f2c43ab216b
Timur Kristóf <venemo@msn.com>
2013-12-10 21:48:56 +0100

    adding Vietnamese + updating translations

--- translations/puzzle-master_vi_VN.ts
+++ translations/puzzle-master_vi_VN.ts
@@ -1,242 +1,243 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="pl">
+<TS version="2.0" language="vi">
 <context>
     <name>AppWindow</name>
     <message>
         <source>You&apos;re a winner!</source>
-        <translation>Jesteś zwycięzcą!</translation>
+        <translation>Bạn là người chiến thắng!</translation>
     </message>
     <message>
         <source>Congratulations, you have successfully solved the given puzzle.</source>
-        <translation>Gratulacje, pomyślnie rozwiązałeś zadane puzzle.</translation>
+        <translation>Chúc mừng, bạn đã thành công trong việc hoàn tất phần giải đố đã đưa ra.</translation>
     </message>
     <message>
         <source>Play again</source>
-        <translation>Zagraj ponownie</translation>
+        <translation>Chơi lại</translation>
     </message>
     <message>
         <source>Choose other</source>
-        <translation>Wybierz inny</translation>
+        <translation>Chọn một loại khác</translation>
     </message>
     <message>
         <source>Are you sure?</source>
-        <translation>Jesteś pewien?</translation>
+        <translation>Bạn chắc chứ?</translation>
     </message>
     <message>
         <source>Do you wish to quit the game?</source>
-        <translation>Czy chcesz zakończyć grę?</translation>
+        <translation>Bạn có muốn thoát khỏi trò chơi này không?</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation>Tak</translation>
+        <translation>Đồng ý</translation>
     </message>
     <message>
         <source>No</source>
-        <translation>Nie</translation>
+        <translation>Không đồng ý</translation>
     </message>
     <message>
         <source>About Puzzle Master %1</source>
-        <translation>O Puzzle Master %1</translation>
+        <translation>Thông tin về chương trình %1</translation>
     </message>
     <message>
         <source>Brought to you by Timur Kristóf.
 Puzzle Master is a fun and addictive jigsaw puzzle game.
 Using Qt.</source>
-        <translation>Stworzone przez Timura Kristófa.
-Puzzle Master to zabawna i wciągająca gra w układanie puzzli.</translation>
+        <translation>Chuyển ngữ bởi Phan Anh.
+Puzzle Master là trò chơi giải đố dạng xếp hình.
+Sử dụng Qt.</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation>Ok</translation>
+        <translation>Đồng ý</translation>
     </message>
     <message>
         <source>Isn&apos;t this a bit difficult?</source>
-        <translation>Nie jest to zbyt trudne?</translation>
+        <translation>Chẳng phải điều này sẽ khó khăn hơn sao?</translation>
     </message>
     <message>
         <source>The selected options will make the game VERY difficult.
 Are you sure to proceed?</source>
-        <translation>Wybrane opcje spowodują, że gra będzie BARDZO trudna.
-Jesteś pewien, że chcesz kontynuować?</translation>
+        <translation>Các tùy chỉnh được chọn sẽ làm màn chơi này trở nên RẤT khó.
+Bạn có chắc là muốn tiếp tục không?</translation>
     </message>
     <message>
         <source>You have already added the selected image.</source>
-        <translation>Już dodałeś wybrany obrazek.</translation>
+        <translation>Bạn đã thêm vào hình ảnh trước đó.</translation>
     </message>
 </context>
 <context>
     <name>GallerySelectorDialog</name>
     <message>
         <source>Select a picture</source>
-        <translation>Wybierz obrazek</translation>
+        <translation>Chọn một hình ảnh</translation>
     </message>
     <message>
         <source>Select</source>
-        <translation>Wybierz</translation>
+        <translation>Chọn</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation>Anuluj</translation>
+        <translation>Hủy bỏ</translation>
     </message>
 </context>
 <context>
     <name>GameBoard</name>
     <message>
         <source>Creating puzzle piece %1 of %2</source>
-        <translation>Tworzenie kawałka puzli %1 z %2</translation>
+        <translation>Đang tạo các mảnh xếp hình %1 của %2</translation>
     </message>
     <message>
         <source>The selected image is being processed.</source>
-        <translation>Wybrany obrazek jest w trakcie przetwarzania.</translation>
+        <translation>Đang xử lý hình ảnh vừa được chọn.</translation>
     </message>
     <message>
         <source>Puzzle Master</source>
-        <translation>Puzzle Master</translation>
+        <translation>Giải Đố Cấp Cao</translation>
     </message>
     <message>
         <source>Wait...</source>
-        <translation>Czekaj...</translation>
+        <translation>Xin vui lòng đợi...</translation>
     </message>
     <message>
         <source>Continue game</source>
-        <translation>Kontynuj grę</translation>
+        <translation>Tiếp tục màn chơi</translation>
     </message>
     <message>
         <source>Get a hint!</source>
-        <translation>Zobacz podpowiedź!</translation>
+        <translation>Nêu gợi ý!</translation>
     </message>
     <message>
         <source>Restart</source>
-        <translation>Restart</translation>
+        <translation>Khởi động lại</translation>
     </message>
     <message>
         <source>Surrender</source>
-        <translation>Poddaj się</translation>
+        <translation>Đầu hàng</translation>
     </message>
     <message>
         <source>About</source>
-        <translation>O grze</translation>
+        <translation>Thông tin</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation>Koniec</translation>
+        <translation>Thoát</translation>
     </message>
     <message>
         <source>Please wait...</source>
-        <translation>Proszę czekać...</translation>
+        <translation>Xin vui lòng đợi...</translation>
     </message>
     <message>
         <source>Game paused</source>
-        <translation>Gra zatrzymana</translation>
+        <translation>Màn chơi bị tạm dừng</translation>
     </message>
     <message>
         <source>Switch back to continue it.</source>
-        <translation>Wróc, żeby kontynuować.</translation>
+        <translation>Quay trở lại và tiếp tục.</translation>
     </message>
     <message>
         <source>Are you sure?</source>
-        <translation>Jesteś pewien?</translation>
+        <translation>Bạn có chắc không?</translation>
     </message>
     <message>
         <source>Do you want to give up this game?
 You can&apos;t continue it then. Use &quot;Get a hint!&quot; if you need a clue.</source>
-        <translation>Czy chcesz się poddać w tej grze?
-Nie będziesz mógł jej kontynuować wtedy. Użyj &quot;Zobacz podpowiedź&quot; jeśli potrzebujesz wskazówki.</translation>
+        <translation>Bạn có muốn bỏ qua màn chơi này không?
+Bạn sẽ không thể tiếp tục chơi tiếp màn chơi này. Click vào &quot;Nêu Gợi Ý&quot; để có thêm gợi ý.</translation>
     </message>
     <message>
         <source>Yes</source>
-        <translation>Tak</translation>
+        <translation>Đồng ý</translation>
     </message>
     <message>
         <source>No</source>
-        <translation>Nie</translation>
+        <translation>Không đồng ý</translation>
     </message>
     <message>
         <source>Do you want to restart this game?</source>
-        <translation>Czy chcesz zrestartować tę grę?</translation>
+        <translation>Bạn có muốn khởi động lại màn chơi này không?</translation>
     </message>
     <message>
         <source>An error has occoured</source>
-        <translation>Wystąpił błąd</translation>
+        <translation>Đã xảy ra lỗi</translation>
     </message>
     <message>
         <source>Sorry, we couldn&apos;t start the game. Please try to start it with another picture.</source>
-        <translation>Przepraszamy, nie mogiliśmy rozpocząć gry. Spróbuj z innym obrazkiem.</translation>
+        <translation>Rất tiếc, chúng tôi không thể bắt đầu màn chơi. Xin hãy thử lại với một hình ảnh khác.</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation>Ok</translation>
+        <translation>Đồng ý</translation>
     </message>
 </context>
 <context>
     <name>ImageChooser</name>
     <message>
         <source>Welcome! Choose an image.</source>
-        <translation>Witaj! Wybierz obrazek.</translation>
+        <translation>Hãy chọn một tấm hình.</translation>
     </message>
     <message>
         <source>Please choose</source>
-        <translation>Proszę wybrać</translation>
+        <translation>Xin vui lòng chọn</translation>
     </message>
     <message>
         <source>You must choose an image before continuing.</source>
-        <translation>Musisz wybrać obrazek przed kontynuacją.</translation>
+        <translation>Bạn phải chọn một tấm hình trước khi tiếp tục.</translation>
     </message>
     <message>
         <source>Ok</source>
-        <translation>Ok</translation>
+        <translation>Đồng ý</translation>
     </message>
     <message>
         <source>Puzzle Master</source>
-        <translation>Puzzle Master</translation>
+        <translation>Giải Đố Cấp Cao</translation>
     </message>
     <message>
         <source>About</source>
-        <translation>O grze</translation>
+        <translation>Thông tin</translation>
     </message>
     <message>
         <source>Quit</source>
-        <translation>Zakończ</translation>
+        <translation>Thoát</translation>
     </message>
 </context>
 <context>
     <name>OptionsDialog</name>
     <message>
         <source>Game difficulty</source>
-        <translation>Trudność gry</translation>
+        <translation>Độ khó của màn chơi</translation>
     </message>
     <message>
         <source>Play</source>
-        <translation>Graj</translation>
+        <translation>Chơi</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation>Anuluj</translation>
+        <translation>Hủy bỏ</translation>
     </message>
     <message>
         <source>Columns</source>
-        <translation>Kolumny</translation>
+        <translation>Cột</translation>
     </message>
     <message>
         <source>Rows</source>
-        <translation>Wiersze</translation>
+        <translation>Dòng</translation>
     </message>
     <message>
         <source>Snap</source>
-        <translation>Przyciągnie</translation>
+        <translation>Snap</translation>
     </message>
     <message>
         <source>Easy</source>
-        <translation>Łatwe</translation>
+        <translation>Dễ</translation>
     </message>
     <message>
         <source>Normal</source>
-        <translation>Normalne</translation>
+        <translation>Bình thường</translation>
     </message>
     <message>
         <source>Difficult</source>
-        <translation>Trudne</translation>
+        <translation>Khó</translation>
     </message>
 </context>
 </TS>
